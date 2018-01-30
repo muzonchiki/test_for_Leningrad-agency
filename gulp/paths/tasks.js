591fc492f07fe9a2f4de818cee6627d1b0fa0ba0
muzonchiki <muzonchiki@yandex.ru>
2018-01-30 13:08:36 +0400

    first commit

---
+++ gulp/paths/tasks.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = [
+  './gulp/tasks/sass.js',
+  './gulp/tasks/serve.js',
+  './gulp/tasks/pug.js',
+  './gulp/tasks/watch.js',
+  './gulp/tasks/clean.js',
+  './gulp/tasks/css.foundation.js'
+];
+
---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+'use strict';
+
+global.$ = {
+  package: require('./package.json'),
+  config: require('./gulp/config'),
+  path: {
+    task: require('./gulp/paths/tasks.js'),
+    cssFoundation: require('./gulp/paths/css.foundation.js'),
+  },
+  gulp: require('gulp'),
+  del: require('del'),
+  browserSync: require('browser-sync').create(),
+  gp: require('gulp-load-plugins')()
+};
+
+$.path.task.forEach(function(taskPath) {
+  require(taskPath)();
+});
+
+$.gulp.task('default', $.gulp.series(
+  'clean',
+  $.gulp.parallel(
+    'sass',
+    'pug',
+    'css:foundation'
+  ),
+  $.gulp.parallel(
+    'watch',
+    'serve'
+  )
+));
